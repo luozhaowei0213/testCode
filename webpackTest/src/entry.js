f8d49f786ca9deb7f358ee0f74e78f606fbd909c
Mrluo <805111216@qq.com>
2018-12-20 01:44:56 +0800

    webpackTest

---
+++ webpackTest/src/entry.js
@@ -0,0 +1,4 @@
+import css from './css/index.css';
+document.getElementById('test').innerHTML='123456';
+
+//打包css  style-loader css-loader
---
+++ webpackTest/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+module.exports = {
+    //入口
+    entry : {
+        common : './src/entry.js',
+        index : './src/entry2.js'
+    },
+    //出口
+    output : {
+        path : path.resolve(__dirname,'dist'),
+        filename : '[name].js'//[name]表示为entry定义入口名称[common||index]
+    },
+    //模块
+    module : {
+
+        //规则 可以有多个
+        rules : [
+            {
+                test : /\.css$/,//找到css文件
+                use : ['style-loader','css-loader']//使用哪些loader
+                //include : ,//需要配置哪些文件，文件夹，扩展名 需要处理
+                //exclude :,//哪些不需要处理
+                //query : //额外配置项 可选
+            }
+        ]
+    },
+    //插件
+    plugins : [],
+
+    //webpack开发服务 热更新 webpack-dev-server
+    devServer : {
+        contentBase : path.resolve(__dirname,'dist'),//基本目录结构，监听哪里
+        host : '192.168.100.6',//ip localhost
+        compress : true,//服务器压缩
+        port : 1717 //端口
+    }
+}
